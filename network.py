8839d90c19e4d21753a4923dc03facce8c14baef
ttk <clarte.lucas@gmail.com>
2020-01-25 16:32:29 +0100

    added first version of neural network from article

--- network.py
+++ network.py
@@ -1,7 +1,8 @@
 # network.py
 # Fichier pour générer les réseaux à l'aide de keras
 
-from tensorflow import keras
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 
 class NetworkFactory:
     def __init__(self):
@@ -11,13 +12,31 @@ class NetworkFactory:
         pass
 
 class BaseNetworkFactory(NetworkFactory):
+    """
+    Reseau propose dans l'article
+    """
     def __init__(self):
-        pass
+        self.input_shape = (1, 7, 500)
+        self.activation = 'relu'
+        self.fsize      = 2
+        self.nfilters1  = 300
+        self.nfilters2  = 50     
+        self.dense_shape = 7*500
 
     def get_network(self):
         model = keras.models.Sequential()
+        model.add(Conv2D(self.nfilters1, self.fsize, activation=self.activation, input_shape=self.input_shape, data_format='channels_first'))
+        model.add(MaxPooling2D())
+        model.add(Dropout(0.5))
+        model.add(Conv2D(self.nfilters2, self.fsize, activation=self.activation))
+        model.add(MaxPooling2D())
+        model.add(Dropout(0.5))
+        model.add(Flatten())
+        model.add(Dense(self.dense_shape, activation=self.activation))
+        model.add(Dense(2, activation='softmax'))
         return model
+
 
 fac = BaseNetworkFactory()
 model = fac.get_network()
-print(model)
+print(model.summary())
