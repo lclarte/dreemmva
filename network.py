119ced90e99b78939851c15bc46780185a609f0e
ttk <clarte.lucas@gmail.com>
2020-01-25 18:23:45 +0100

    fixed network from article

--- network.py
+++ network.py
@@ -16,16 +16,17 @@ class BaseNetworkFactory(NetworkFactory):
     Reseau propose dans l'article
     """
     def __init__(self):
-        self.input_shape = (1, 7, 500)
+        # need to put channel last otherwise there are bugs
+        self.input_shape = (7, 500, 40)
         self.activation = 'relu'
         self.fsize      = 2
         self.nfilters1  = 300
         self.nfilters2  = 50     
-        self.dense_shape = 7*500
+        self.dense_shape = 40*7*500
 
     def get_network(self):
         model = keras.models.Sequential()
-        model.add(Conv2D(self.nfilters1, self.fsize, activation=self.activation, input_shape=self.input_shape, data_format='channels_first'))
+        model.add(Conv2D(self.nfilters1, self.fsize, activation=self.activation, input_shape=self.input_shape, data_format='channels_last'))
         model.add(MaxPooling2D())
         model.add(Dropout(0.5))
         model.add(Conv2D(self.nfilters2, self.fsize, activation=self.activation))
@@ -35,8 +36,3 @@ class BaseNetworkFactory(NetworkFactory):
         model.add(Dense(self.dense_shape, activation=self.activation))
         model.add(Dense(2, activation='softmax'))
         return model
-
-
-fac = BaseNetworkFactory()
-model = fac.get_network()
-print(model.summary())
