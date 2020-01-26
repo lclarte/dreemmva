cbb342c9a8f6becc1b5354d65ab8c50aa2057ef7
ttk <clarte.lucas@gmail.com>
2020-01-26 12:13:07 +0100

    functional version of master w/ SimpleNetworkFactory

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ def main():
 	y_train = np.array(load_y('data/y_train.csv'))
 	x_train, y_train = flatten_data(x_train, y_train)
 	reorder_nhwc(x_train)
-	print(x_train.shape, y_train.shape)
-	input()
 
 	# load network and compile it 
-	model = BaseNetworkFactory().get_network()
+	model = SimpleNetworkFactory().get_network()
 	print(model.summary())
 	model.compile(loss='binary_crossentropy', optimizer='adamax', metrics=['accuracy'])
 
