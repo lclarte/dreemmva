f9c8ef99727be6deae70fcb0268178f9b6dbd521
ttk <clarte.lucas@gmail.com>
2020-01-26 16:25:06 +0100

    Added Conv1DNetworkFactory. Network w/ only 1D convolutions

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from data import *
 from network import *
 
+from sklearn import model_selection
+
 def main():
 	# test that we can load data
-	x_train = np.array(load_x('data/X_train.h5'))
-	y_train = np.array(load_y('data/y_train.csv'))
-	x_train, y_train = flatten_data(x_train, y_train)
-	reorder_nhwc(x_train)
-	print(x_train.shape, y_train.shape)
-	input()
+	X = np.array(load_x('data/X_train.h5'))
+	Y = np.array(load_y('data/y_train.csv'))
+	X, Y = flatten_data(X, Y)
+	reorder_nhwc(X)
 
 	# load network and compile it 
 	model = BaseNetworkFactory().get_network()
 	print(model.summary())
+	input("Press a key to continue ... ")
+	
 	model.compile(loss='binary_crossentropy', optimizer='adamax', metrics=['accuracy'])
 
-	model.fit(x_train, y_train, epochs=10, batch_size=32)
+	size = 1000
+	X, Y = X[:size], Y[:size]
+	model.fit(X, Y, epochs=50, batch_size=32, validation_split=0.25)
 
 if __name__=='__main__':
 	main()
