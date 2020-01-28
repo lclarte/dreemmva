b7c1a739a2028e0692d14b172c6b5601379d1ffa
ttk <clarte.lucas@gmail.com>
2020-01-28 21:16:57 +0100

    added small changes

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from core.data import *
 from core.network import *
 from core.visualization import *
+from core.model import *
 
 from sklearn import model_selection
 
 def test_train():
 	# test that we can load data
 	X, Y = load_all('data/X_train.h5', 'data/y_train.csv')
+	print('Shape of the data :')
+	print("X.shape = ", X.shape)
+	print("Y.shape = ", Y.shape)
 
+	# for testing : what if we work on Fourier Transform ?
 	X = fft_eeg(X)
 
-	# load network and compile it 
-	model = Conv1DNetworkFactory().get_network()
-	print("Number of samples", X.shape[0])
-	input("Press a key to continue ... ")
-	
-	model.compile(loss='binary_crossentropy', optimizer='adamax', metrics=['accuracy'])
-
-	model.fit(X, Y, epochs=50, batch_size=32, validation_split=0.25)
+	model = NNModel(None)
+	model.train(X, Y)
 
 if __name__=='__main__':
 	test_train()
