08e3e3b3f57e320fa2cf8e4e27d98eeb04b1fdf0
lucas <lucas@DESKTOP-FQ43C8S.localdomain>
2020-01-30 01:53:42 +0100

    changed main to add prediction

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn import model_selection
 def test_train():
 	# test that we can load data
 	X, Y = load_all('data/X_train.h5', 'data/y_train.csv')
+	X_test = load_x('data/X_train.h5')
 	print('Shape of the data :')
 	print("X.shape = ", X.shape)
 	print("Y.shape = ", Y.shape)
@@ -16,7 +17,11 @@ def test_train():
 	X = fft_eeg(X)
 
 	model = NNModel(None)
+	X, Y = X[:50], Y[:50]
+
 	model.train(X, Y)
+	y_pred = model.predict(X)
+	print("Accuracy = ", compare_predict(y_pred, Y))
 
 if __name__=='__main__':
 	test_train()
