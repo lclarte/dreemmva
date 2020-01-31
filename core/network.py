2d350326484b92aa5c39b9aaa08dab37b6b56856
lucas <lucas@DESKTOP-FQ43C8S.localdomain>
2020-02-01 00:09:25 +0100

    removed Conv1D network and changed others

--- core/network.py
+++ core/network.py
@@ -43,41 +43,25 @@ class BaseNetworkFactory(NetworkFactory):
     def __init__(self):
         # need to put channel last otherwise there are bugs
         super().__init__()
-        self.fsize      = 2
         self.nfilters1  = 100
-        self.nfilters2  = 10    
+        self.nfilters2  = 300
         self.dense_shape = self.h*self.w*self.c
 
     def get_network(self):
         model = keras.models.Sequential()
-        model.add(Conv2D(self.nfilters1, self.fsize, activation=self.activation, input_shape=self.input_shape, data_format='channels_last'))
-        model.add(MaxPooling2D())
+        model.add(Conv2D(self.nfilters1, kernel_size=(3, 3), activation=self.activation, input_shape=self.input_shape, data_format='channels_last'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(self.nfilters1, kernel_size=(2, 3), activation=self.activation))
+        model.add(MaxPooling2D(pool_size=(1, 2)))
         model.add(Dropout(0.5))
-        model.add(Conv2D(self.nfilters2, self.fsize, activation=self.activation))
-        model.add(MaxPooling2D())
-        model.add(Dropout(0.5))
+        model.add(Conv2D(self.nfilters2, kernel_size=(1, 7), activation=self.activation))
+        model.add(MaxPooling2D(pool_size=(1, 2)))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(self.nfilters1, kernel_size=(1, 3), activation=self.activation))
+        model.add(Conv2D(self.nfilters1, kernel_size=(1, 3), activation=self.activation))
         model.add(Flatten())
         model.add(Dense(self.dense_shape, activation=self.activation))
         # output = vecteur de probas pour chaque classe
-        model.add(Dense(2, activation='softmax'))
-        return model
-
-class Conv1DNetworkFactory(NetworkFactory):
-    def __init__(self):
-        super().__init__()
-        self.fsize = [2, 2, 2, 2, 2, 2, 2]
-        self.nfilters = [300, 300, 200, 200, 100, 100, 100]
-        self.dense_shape = self.h * self.w * self.c
-
-    def get_network(self):
-        model = keras.models.Sequential()
-        # reshape to remove channel dim, permute h and w because we have channel last
-        model.add(Reshape((self.w, self.h), input_shape=self.input_shape))
-        for i in range(0, len(self.fsize)):
-            model.add(Conv1D(self.nfilters[i], self.fsize[i], activation=self.activation, data_format='channels_last'))
-            model.add(MaxPooling1D(2))
-            model.add(Dropout(0.5))
-        model.add(Flatten())
-        model.add(Dense(self.dense_shape, activation=self.activation))
         model.add(Dense(2, activation='softmax'))
         return model
