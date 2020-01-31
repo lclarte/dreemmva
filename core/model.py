f86cb22c9b81f0ce1358074d3616c6031fb6c1c9
lucas <lucas@DESKTOP-FQ43C8S.localdomain>
2020-02-01 00:09:36 +0100

    cleaned model class

--- core/model.py
+++ core/model.py
@@ -1,7 +1,7 @@
 # model.py
 # classe abstraite qui inclut les NN et autres modeles (modeles lineaires)
 
-from core.network import BaseNetworkFactory
+from core.network import *
 import core.data as data
 import numpy as np
 
@@ -26,15 +26,17 @@ class NNModel(TrainableModel):
         self.init(config)
 
     def init(self, config):
+        factories = {'SimpleNetworkFactory' : SimpleNetworkFactory, 'BaseNetworkFactory' : BaseNetworkFactory}
         # define useful constants
-        self.epoch            = 3
+        self.epoch            = config['epoch'] 
+        self.optimizer        = config['optimizer']
         self.batch_size       = 32
         self.validation_split = 0.25
 
         # initialize NN
-        factory = BaseNetworkFactory()
+        factory = factories[config['nn_factory']]()
         self.network = factory.get_network()
-        self.network.compile(loss='binary_crossentropy', optimizer='adamax', metrics=['accuracy'])
+        self.network.compile(loss='binary_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
 
     def train(self, x_train, y_train):
         """
@@ -43,8 +45,11 @@ class NNModel(TrainableModel):
         """
         x_train, y_train = data.flatten_data(x_train, y_train)
         x_train = data.reorder_nhwc(x_train)
+        # x_train = data.fft_eeg(x_train)
 
-        self.network.fit(x_train, y_train, epochs=self.epoch, batch_size=self.batch_size, validation_split=self.validation_split)
+        weights = data.get_sample_weights(y_train)
+
+        self.network.fit(x_train, y_train, epochs=self.epoch, batch_size=self.batch_size, validation_split=self.validation_split, sample_weight=weights)
         return True
 
     def set_network(self, network):
@@ -64,7 +69,7 @@ class NNModel(TrainableModel):
         y = np.zeros(shape=(n, 2))
         for i in range(n):
             x = x_test[i].reshape((1, c, h, w))
-            x = data.reorder_nhwc(data.flatten_x(x))
+            x = data.fft_eeg(data.reorder_nhwc(data.flatten_x(x)))
             # set of 40 predictions, one by independant sample
             predictions = self.network.predict(x)
             indx = np.argmax(np.mean(predictions, axis=0))
